4f293bd5dda318430af40b05c0f0212d3e68bc53
dwl5 <ssop6403@gmail.com>
2023-05-14 00:47:36 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편

---
+++ jpa/jpa/src/main/java/hellojpa/JpaCreate.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaCreate {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+
+            /*
+            member.setId(1L);
+            member.setName("HelloA");
+            */
+
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        // 프로그램이 종료될 때 닫아줘야한다.
+        emf.close();
+    }
+}
