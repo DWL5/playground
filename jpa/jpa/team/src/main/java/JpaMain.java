0b049a4420f243a1adae0515978ae3c6e7f5fe5a
dwl5 <ssop6403@gmail.com>
2023-05-20 18:32:10 +0900

    20230520 다양한 연관관계 매핑 공부

--- jpa/jpa/team/src/main/java/JpaMain.java
+++ jpa/jpa/team/src/main/java/JpaMain.java
@@ -2,7 +2,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -12,29 +11,14 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
             Member member = new Member();
             member.setUsername("member1");
-            member.changeTeam(team);
             em.persist(member);
 
-/*
+            Team team = new Team();
+            team.setName("teamA");
             team.getMembers().add(member);
-
-            em.flush();
-            em.clear();*/
-
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
-
-            System.out.println("=====================");
-            for (Member m : members) {
-                System.out.println("m = " + m.getUsername());
-            }
-            System.out.println("=====================");
+            em.persist(team);
 
             tx.commit();
         } catch (Exception e) {
---
+++ jpa/jpa/team/src/main/java/Locker.java
@@ -0,0 +1,14 @@
+import javax.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    @Column(name = "LOCKER_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Locker locker;
+}
