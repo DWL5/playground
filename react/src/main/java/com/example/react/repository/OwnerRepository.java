57fc354ec1e93d5ed521917e1d34e67ac98d3fa2
DWL5 <ssop6403@gmail.com>
2023-10-31 23:32:01 +0900

    공부안하는거 삭제

--- react/src/main/java/com/example/react/repository/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.react.repository;
-
-import com.example.react.domain.Owner;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OwnerRepository extends CrudRepository<Owner, Long> {
-}
